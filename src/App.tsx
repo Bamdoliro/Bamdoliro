1735f9f4dd527100cd8840141755d45cbbc4fbe1
SEOKKAMONI <sj01048260279@gmail.com>
2023-01-02 09:40:49 +0900

    ADD :: global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import * as P from './Pages';
+import GlobalStyled from 'Shared/Styles/GlobalStyle';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<P.Main />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<P.Main />} />
+        </Routes>
+      </BrowserRouter>
+      <GlobalStyled />
+    </>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
